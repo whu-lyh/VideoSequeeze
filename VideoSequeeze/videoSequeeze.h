5ad00e74b588475973a1d34496367b91cd3f76fc
whu-lyh <2942563671@qq.com>
2020-02-29 20:54:25 +0800

    init repo

---
+++ VideoSequeeze/videoSequeeze.h
@@ -0,0 +1,17 @@
+#pragma once
+//this project is used to sequeeze a long video to a n times faster
+
+#pragma warning(disable: 4267)
+#pragma warning(disable: 4251)
+#pragma warning(disable: 4267)
+#pragma warning(disable: 4244)
+
+#ifdef _DEBUG
+#pragma comment(lib,"")
+#pragma comment(lib,"")
+#pragma comment(lib,"")
+#else
+#pragma comment(lib,"")
+#pragma comment(lib,"")
+#pragma comment(lib,"")
+#endif
